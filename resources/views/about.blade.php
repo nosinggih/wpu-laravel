bb0ed5a5f72ff6fc579149de661f1b3982a4edd1
nursinggih <nosinggih@gmail.com>
2020-10-15 21:12:13 +0700

    intial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title','About')
+
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <col-10>
+            <h1 class="mt-3">Hello {{$nama}}!</h1>
+        </col-10>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title','Web Programming UNPAS')
+
+
+@section('container')
+<div class="container">
+  <div class="row">
+    <col-10>
+      <h1 class="mt-3">Hello World!</h1>
+    </col-10>
+  </div>
+</div>
+@endsection
